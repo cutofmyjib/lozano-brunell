74878604c43477f7161c6a8189a2a4cf24171b41
cutofmyjib <thedianalozano@gmail.com>
2016-04-15 16:16:47 -0700

    wording

--- main/app/home.js
+++ main/app/home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
       <div className='main'>
         <h1 className='main-title'>CELESTE &amp; MATT</h1>
         <p>Saturday, October 29, 2016</p>
-        <p className='p-blurb'>Friends and Family! Welcome to our digital home of Celeste and Matt's wedding! We are excited to celebrate our wedding on The 29th of October with everyone! Thank you for setting Facebook aside or Instagram or your work email to check out our website! Explore around our site and don't forget to RSVP!</p>
+        <p className='p-blurb'>Friends and Family! Welcome to our digital home of Celeste and Matt's wedding! We are excited to celebrate our wedding on the 29th of October with everyone! Thank you for setting Facebook, Instagram, your work email aside to check out our website! Please explore and don't forget to RSVP!</p>
       </div>
     )
   }
